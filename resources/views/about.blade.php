df0b8cc135c7cfc649f542cb598cb4117c86ca8a
yasmineNov <ynh025@gmail.com>
2023-07-08 16:26:36 +0700

    fix bottom header

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
-    <div class="bg-light py-3">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 mb-0"><a class="nav-link" href="/home">Home</a> <span>/</span> <strong class="text-black">About</strong></div>
-        </div>
-      </div>
-    </div>  
+<div class="bg-light py-3">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12 mb-0"><a href="/">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">About</strong></div>
+    </div>
+  </div>
+</div>
 
     <div class="site-section border-bottom" data-aos="fade">
       <div class="container">
--- resources/views/artikel.blade.php
+++ resources/views/artikel.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.main')
 
 @section('container')
+<div class="bg-light py-3">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 mb-0"><a href="/">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Artikel</strong></div>
+      </div>
+    </div>
+  </div>
+
  <h1>Ini Adalah halaman Artikel test</h1>
 @endsection
