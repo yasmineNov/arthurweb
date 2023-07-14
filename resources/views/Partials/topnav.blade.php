05773cc12293513b12103a1c06ef28f99a4acb94
yasmineNov <ynh025@gmail.com>
2023-07-14 19:26:16 +0700

    Add link all sosmed

--- resources/views/Partials/topnav.blade.php
+++ resources/views/Partials/topnav.blade.php
@@ -5,9 +5,9 @@
       <div class="col-md-6">
         <ul class="site-menu d-flex align-items-center justify-content-start">
           <li class="nav-item"><a class="nav-link text-dark" href="https://wa.me/087858860888"><img src="images/whatsapp.png"></a></li>
-          <li class="nav-item"><a class="nav-link text-dark" href="#"><img src="images/instagram.png"></a></li>
-          <li class="nav-item"><a class="nav-link text-dark" href="#"><img src="images/shopee.png"></a></li>
-          <li class="nav-item"><a class="nav-link text-dark" href="#"><img src="images/tokped.png"></a></li>
+          <li class="nav-item"><a class="nav-link text-dark" href="https://www.instagram.com/arthurcitramedia/"><img src="images/instagram.png"></a></li>
+          <li class="nav-item"><a class="nav-link text-dark" href="https://shopee.co.id/arthuradv?smtt=0.0.9"><img src="images/shopee.png"></a></li>
+          <li class="nav-item"><a class="nav-link text-dark" href="https://www.tokopedia.com/archive-arthuradv"><img src="images/tokped.png"></a></li>
         </ul>
       </div>
       <div class="col-md-6">
