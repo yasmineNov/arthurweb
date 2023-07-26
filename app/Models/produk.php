d4115f1d95096e780b206b0c14e0516753434477
DedenFirmansyah <dedenf98@gmail.com>
2023-07-26 21:33:09 +0700

    menambahkan edit produk
    tapi masih error hehhe

--- app/Models/produk.php
+++ app/Models/produk.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class produk extends Model
 {
     use HasFactory;
+    protected $primaryKey = 'idProduk';
+    protected $fillable = ['idProduk', 'img', 'namaProduk', 'harga', 'deskripsi', 'kategori'];
+    protected $table = 'produks';
+    public $timestamps = false;
 }
