0184fd0c6699d8aa23f1c79317e0037c4dcecf61
DedenFirmansyah <dedenf98@gmail.com>
2023-08-12 01:10:14 +0700

    pereditan sudah diperbaiki gaes

--- resources/views/layouts/mainadmin.blade.php
+++ resources/views/layouts/mainadmin.blade.php
@@ -15,8 +15,8 @@
 
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-
-        <link href="css/styles-admin.css" rel="stylesheet" />
+        <link href="{{ asset('css/styles-admin.css') }}" rel="stylesheet">
+        {{-- <link href="css/styles-admin.css" rel="stylesheet" /> --}}
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
