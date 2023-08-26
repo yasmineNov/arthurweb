f6ae8b2c225f234028a4fc370b2c92617b655a77
yasmineNov <ynh025@gmail.com>
2023-08-26 18:40:42 +0700

    Fix content show on tinyMCE

--- resources/views/artikel-single.blade.php
+++ resources/views/artikel-single.blade.php
@@ -9,15 +9,15 @@
       </div>
     </div>
   </div>
-  <a class="btn btn-primary mt-5" href="/frontartikel" role="button"><i class="fa-solid fa-circle-arrow-left"></i> Kembali</a>
+  <a class="btn btn-primary mt-3" href="/frontartikel" role="button"><i class="fa-solid fa-circle-arrow-left"></i> Kembali</a>
 
-  <div class="text-center title-artikel mt-4 mb-3">
+  <div class="text-center title-artikel mt-3 mb-3">
     <h1>{{$artikel->judul}}</h1>
   </div>
   <div class="text-center mt-4 mb-3">             
     <img src="{{asset('storage/image-artikel/'.$artikel->img)}}" style="aspect-ratio: 11 / 8" class="img-artikel" alt="...">
   </div>
   <div class="text-right des-title mb-5">
-    <p class="detail-artikel">{{($artikel->konten)}}</p>
+    <p class="detail-artikel">{!!$artikel->konten!!}</p>
   </div>
  @endsection
