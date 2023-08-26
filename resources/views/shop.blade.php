5ad2ad0a183acc29716d519a1fade1f6f6a57eb5
yasmineNov <ynh025@gmail.com>
2023-08-26 18:35:55 +0700

    little fix artikel

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -4,7 +4,7 @@
     <div class="bg-light py-3">
       <div class="container">
         <div class="row">
-          <div class="col-md-12 mb-0"><a href="/">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Keranjang</strong></div>
+          <div class="col-md-12 mb-0"><a href="/">Home</a> <span class="mx-2 mb-0">/</span> <strong class="text-black">Katalog Produk</strong></div>
         </div>
       </div>
     </div>
