51d7ce797b1c4c3c962734d16601f3df585c9554
DedenFirmansyah <dedenf98@gmail.com>
2023-08-26 08:48:51 +0700

    ngedit home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -233,7 +233,7 @@
     </div>
   </div>
 
-<div class="site-section block-8">
+{{-- <div class="site-section block-8">
   <div class="container">
     <div class="row justify-content-center  mb-5">
       <div class="col-md-7 site-section-heading text-center pt-4">
@@ -252,5 +252,168 @@
       </div>
     </div>
   </div>
-</div>
+</div> --}}
+<!-- Features -->
+<section>
+  <div class="container">
+    <div class="card p-4 bg-primary">
+      <div class="row align-items-center">
+        <div class="col">
+          <h4 class="mb-0 text-white">Best products and brands in store</h4>
+          <p class="mb-0 text-white-50">Trendy products and text to build on the card title</p>
+        </div>
+        <div class="col-auto"><a class="btn btn-white text-primary shadow-0" href="#">Discover</a></div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Features -->
+
+<!-- Recommended -->
+<section>
+  <div class="container my-5">
+    <header class="mb-4">
+      <h3>Recommended</h3>
+    </header>
+
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card my-2 shadow-0">
+          <a href="#" class="">
+            <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/9.webp" class="card-img-top rounded-2" style="aspect-ratio: 1 / 1"/>
+          </a>
+          <div class="card-body p-0 pt-3">
+            <a href="#!" class="btn btn-light border px-2 pt-2 float-end icon-hover"><i class="fas fa-heart fa-lg px-1 text-secondary"></i></a>
+            <h5 class="card-title">$17.00</h5>
+            <p class="card-text mb-0">Blue jeans shorts for men</p>
+            <p class="text-muted">
+              Sizes: S, M, XL
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card my-2 shadow-0">
+          <a href="#" class="">
+            <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/10.webp" class="card-img-top rounded-2"style="aspect-ratio: 1 / 1" />
+          </a>
+          <div class="card-body p-0 pt-2">
+            <a href="#!" class="btn btn-light border px-2 pt-2 float-end icon-hover"><i class="fas fa-heart fa-lg px-1 text-secondary"></i></a>
+            <h5 class="card-title">$9.50</h5>
+            <p class="card-text mb-0">Slim fit T-shirt for men</p>
+            <p class="text-muted">
+              Sizes: S, M, XL
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card my-2 shadow-0">
+          <a href="#" class="">
+            <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/11.webp" class="card-img-top rounded-2" style="aspect-ratio: 1 / 1"/>
+          </a>
+          <div class="card-body p-0 pt-2">
+            <a href="#!" class="btn btn-light border px-2 pt-2 float-end icon-hover"><i class="fas fa-heart fa-lg px-1 text-secondary"></i></a>
+            <h5 class="card-title">$29.95</h5>
+            <p class="card-text mb-0">Modern product name here</p>
+            <p class="text-muted">
+              Sizes: S, M, XL
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card my-2 shadow-0">
+          <a href="#" class="">
+            <img src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/items/12.webp" class="card-img-top rounded-2" style="aspect-ratio: 1 / 1"/>
+          </a>
+          <div class="card-body p-0 pt-2">
+            <a href="#!" class="btn btn-light border px-2 pt-2 float-end icon-hover"><i class="fas fa-heart fa-lg px-1 text-secondary"></i></a>
+            <h5 class="card-title">$29.95</h5>
+            <p class="card-text mb-0">Modern product name here</p>
+            <p class="text-muted">
+              Material: Jeans
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Recommended -->
+
+{{-- blog --}}
+<section class="mt-5 mb-4">
+  <div class="container text-dark">
+    <header class="mb-4">
+      <h3>Blog posts</h3>
+    </header>
+
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <article>
+          <a href="#" class="img-fluid">
+            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/1.webp" style="object-fit: cover;" height="160" />
+          </a>
+          <div class="mt-2 text-muted small d-block mb-1">
+            <span>
+              <i class="fa fa-calendar-alt fa-sm"></i>
+              23.12.2022
+            </span>
+            <a href="#"><h6 class="text-dark">How to promote brands</h6></a>
+            <p>When you enter into any new area of science, you almost reach</p>
+          </div>
+        </article>
+      </div>
+      <!-- col.// -->
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <article>
+          <a href="#" class="img-fluid">
+            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/2.webp" style="object-fit: cover;" height="160" />
+          </a>
+          <div class="mt-2 text-muted small d-block mb-1">
+            <span>
+              <i class="fa fa-calendar-alt fa-sm"></i>
+              13.12.2022
+            </span>
+            <a href="#"><h6 class="text-dark">How we handle shipping</h6></a>
+            <p>When you enter into any new area of science, you almost reach</p>
+          </div>
+        </article>
+      </div>
+      <!-- col.// -->
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <article>
+          <a href="#" class="img-fluid">
+            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/3.webp" style="object-fit: cover;" height="160" />
+          </a>
+          <div class="mt-2 text-muted small d-block mb-1">
+            <span>
+              <i class="fa fa-calendar-alt fa-sm"></i>
+              25.11.2022
+            </span>
+            <a href="#"><h6 class="text-dark">How to promote brands</h6></a>
+            <p>When you enter into any new area of science, you almost reach</p>
+          </div>
+        </article>
+      </div>
+      <!-- col.// -->
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <article>
+          <a href="#" class="img-fluid">
+            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/4.webp" style="object-fit: cover;" height="160" />
+          </a>
+          <div class="mt-2 text-muted small d-block mb-1">
+            <span>
+              <i class="fa fa-calendar-alt fa-sm"></i>
+              03.09.2022
+            </span>
+            <a href="#"><h6 class="text-dark">Success story of sellers</h6></a>
+            <p>When you enter into any new area of science, you almost reach</p>
+          </div>
+        </article>
+      </div>
+    </div>
+  </div>
+</section>
 @endsection
