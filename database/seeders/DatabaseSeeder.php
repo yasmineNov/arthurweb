e5b0be17727d5f1aebfe6d928585d3931039f699
yasmineNov <ynh025@gmail.com>
2023-10-05 22:00:59 +0700

    fix namekategori

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -52,12 +52,14 @@ class DatabaseSeeder extends Seeder
         produk::create([
             'namaProduk' => 'mug',
             'harga' => '25000',
-            'deskripsi' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,'
+            'deskripsi' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,',
+            'idKategori' => '3',
         ]);
         produk::create([
-            'namaProduk' => 'XBanner',
+            'namaProduk' => 'XBanner Vynil 280gr',
             'harga' => '48000',
-            'deskripsi' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,'
+            'deskripsi' => 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,',
+            'idKategori' => '2',
         ]);
     }
 }
