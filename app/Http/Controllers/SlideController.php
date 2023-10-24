2838e263a266075022ae788f6208987a63a8e4c5
DedenFirmansyah <86413180+DedenFirmansyah@users.noreply.github.com>
2023-10-24 20:59:03 +0700

    kerangka slider

---
+++ app/Http/Controllers/SlideController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\slide;
+use Illuminate\Http\Request;
+
+class SlideController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(slide $slide)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(slide $slide)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, slide $slide)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(slide $slide)
+    {
+        //
+    }
+}
