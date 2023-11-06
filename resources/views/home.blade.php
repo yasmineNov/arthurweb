561669df6c3c97d8d45d2bf4dae3a356b4d6005c
DedenFirmansyah <86413180+DedenFirmansyah@users.noreply.github.com>
2023-11-06 20:38:46 +0700

    fix artikel

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -249,81 +249,24 @@
     </header>
 
     <div class="row">
-      {{-- <div class="col-12"> --}}
-        {{-- <br class="d-flex flex-row flex-nowrap overflow-auto"> --}}
-        {{-- <div class="d-flex justify-content-center"> --}}
             @foreach ($dataPost as $artikel)
               <div class="col-lg-3 col-md-6 col-sm-6 d-flex">
                 <div class="card w-80 my-2 shadow-2-strong">
                   <a href="{{ url('artikel.show',$artikel->idArtikel) }}">
                     <img src="{{asset('storage/image-artikel/'.$artikel->img)}}" class="card-img-top" style="aspect-ratio: 1 / 1" alt="...">
                   </a>
-                  {{-- <div class="mt-2 text-muted small d-block mb-1">
-                    <span>
-                      <i class="fa fa-calendar-alt fa-sm"></i>
-                        23.12.2022
-                      </span> --}}
-                  <div class="card-body d-flex flex-column">
-                  <a href="{{ url('artikel.show',$artikel->idArtikel) }}">
-                    <h5 class="title-artikel text-center mt-2 mb-0">{{$artikel->judul}}</h5>
-                  </a>
-                  <p class="text-center mt-0">{!!substr ($artikel->konten, 0,65)!!}.....</p> 
+                  <div class="mt-2 text-muted small d-block mb-1">
+                    <div class="card-body d-flex flex-column">
+                    <a href="{{ url('artikel.show',$artikel->idArtikel) }}">
+                      <h5 class="title-artikel text-center mt-2 mb-0">{{$artikel->judul}}</h5>
+                    </a>
+                    <p class="text-center mt-0">{!!substr ($artikel->konten, 0,65)!!}.....</p> 
+                    </div>
                   </div>
-                </div>
               </div>
               <br>
+              </div>
             @endforeach 
-        {{-- </div>  --}}
-      {{-- </div> --}}
-    </div>
-      <!-- col.// -->
-      {{-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-        <article>
-          <a href="#" class="img-fluid">
-            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/2.webp" style="object-fit: cover;" height="160" />
-          </a>
-          <div class="mt-2 text-muted small d-block mb-1">
-            <span>
-              <i class="fa fa-calendar-alt fa-sm"></i>
-              13.12.2022
-            </span>
-            <a href="#"><h6 class="text-dark">How we handle shipping</h6></a>
-            <p>When you enter into any new area of science, you almost reach</p>
-          </div>
-        </article>
-      </div>
-      <!-- col.// -->
-      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-        <article>
-          <a href="#" class="img-fluid">
-            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/3.webp" style="object-fit: cover;" height="160" />
-          </a>
-          <div class="mt-2 text-muted small d-block mb-1">
-            <span>
-              <i class="fa fa-calendar-alt fa-sm"></i>
-              25.11.2022
-            </span>
-            <a href="#"><h6 class="text-dark">How to promote brands</h6></a>
-            <p>When you enter into any new area of science, you almost reach</p>
-          </div>
-        </article>
-      </div> --}}
-      <!-- col.// -->
-      {{-- <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-        <article>
-          <a href="#" class="img-fluid">
-            <img class="rounded w-100" src="https://bootstrap-ecommerce.com/bootstrap5-ecommerce/images/posts/4.webp" style="object-fit: cover;" height="160" />
-          </a>
-          <div class="mt-2 text-muted small d-block mb-1">
-            <span>
-              <i class="fa fa-calendar-alt fa-sm"></i>
-              03.09.2022
-            </span>
-            <a href="#"><h6 class="text-dark">Success story of sellers</h6></a>
-            <p>When you enter into any new area of science, you almost reach</p>
-          </div>
-        </article>
-      </div> --}}
     </div>
   </div>
 </section>
